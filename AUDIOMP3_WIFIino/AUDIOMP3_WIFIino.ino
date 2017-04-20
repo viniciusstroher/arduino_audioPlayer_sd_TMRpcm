5d1ec18a985c652a007196d051d3e972a21dccb9
Vinicius <viniciusferreirawk@gmail.com>
2017-04-20 16:51:00 -0300

    fix

--- AUDIOMP3_WIFIino/AUDIOMP3_WIFIino.ino
+++ AUDIOMP3_WIFIino/AUDIOMP3_WIFIino.ino
@@ -8,7 +8,16 @@ TMRpcm tmrpcm;   // create an object for use in this sketch
 /*
 	tem exemplo de uso do SD nos exemplos do arduino IDE
 */
+
+#include "ESP8266.h"
+
+#define SSID        "w3GET"
+#define PASSWORD    "minhavisita"
+
+ESP8266 wifi(Serial1);
+
 void setup(){
+  Serial1.begin(115200);
   Serial.begin(9600);
   tmrpcm.speakerPin = 9; //11 on Mega, 9 on Uno, Nano, etc
  
@@ -16,11 +25,85 @@ void setup(){
     Serial.println("NO SD DETECTED");
   return;   // don't do anything more if not
   }
-  Serial.println("Play audio");
- tmrpcm.volume(1);
- tmrpcm.play("1.wav"); //the sound file "1" will play each time the arduino powers up, or is reset
+    // Serial.println("Play audio");
+    tmrpcm.volume(1);
+   // tmrpcm.play("1.wav"); //the sound file "1" will play each time the arduino powers up, or is reset
+    Serial.begin(9600);
+    Serial.print("setup begin\r\n");
+    
+    Serial.print("FW Version:");
+    Serial.println(wifi.getVersion().c_str());
+      
+    if (wifi.setOprToStationSoftAP()) {
+        Serial.print("to station + softap ok\r\n");
+    } else {
+        Serial.print("to station + softap err\r\n");
+    }
+ 
+    if (wifi.joinAP(SSID, PASSWORD)) {
+        Serial.print("Join AP success\r\n");
+        Serial.print("IP: ");
+        Serial.println(wifi.getLocalIP().c_str());    
+    } else {
+        Serial.print("Join AP failure\r\n");
+    }
+    
+    if (wifi.enableMUX()) {
+        Serial.print("multiple ok\r\n");
+    } else {
+        Serial.print("multiple err\r\n");
+    }
+    
+    if (wifi.startTCPServer(8090)) {
+        Serial.print("start tcp server ok\r\n");
+    } else {
+        Serial.print("start tcp server err\r\n");
+    }
+    
+    if (wifi.setTCPServerTimeout(10)) { 
+        Serial.print("set tcp server timout 10 seconds\r\n");
+    } else {
+        Serial.print("set tcp server timout err\r\n");
+    }
+    
+    Serial.print("setup end\r\n");
 }
  
 void loop(){  
- 
-}
+     uint8_t buffer[128] = {0};
+    uint8_t mux_id;
+    uint32_t len = wifi.recv(&mux_id, buffer, sizeof(buffer), 100);
+    if (len > 0) {
+        Serial.print("Status:[");
+        Serial.print(wifi.getIPStatus().c_str());
+        Serial.println("]");
+        
+        Serial.print("Received from :");
+        Serial.print(mux_id);
+        Serial.print("[");
+        for(uint32_t i = 0; i < len; i++) {
+            Serial.print((char)buffer[i]);
+        }
+        Serial.print("]\r\n");
+        
+        if(wifi.send(mux_id, buffer, len)) {
+            Serial.print("send back ok\r\n");
+        } else {
+            Serial.print("send back err\r\n");
+        }
+        
+        if (wifi.releaseTCP(mux_id)) {
+            Serial.print("release tcp ");
+            Serial.print(mux_id);
+            Serial.println(" ok");
+        } else {
+            Serial.print("release tcp");
+            Serial.print(mux_id);
+            Serial.println(" err");
+        }
+        
+        Serial.print("Status:[");
+        Serial.print(wifi.getIPStatus().c_str());
+        Serial.println("]");
+    }
+} 
