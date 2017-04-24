9ccefafd0b27aae742412f7616d2e247141db66a
Vinicius <viniciusferreirawk@gmail.com>
2017-04-24 15:05:41 -0300

    fix

---
+++ AUDIOMP3_WIFIino/enviaAudio.js
@@ -0,0 +1,17 @@
+
+var net = require('net');
+
+var client = new net.Socket();
+client.connect(8090, '192.168.0.43', function() {
+	console.log('Connected');
+	client.write('Hello, server! Love, Client.');
+});
+
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+	client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
