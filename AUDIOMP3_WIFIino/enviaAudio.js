ffb666213a6a6ba172f6f67a7ec48d68b58e4cc9
Vinicius <viniciusferreirawk@gmail.com>
2017-04-24 15:50:04 -0300

    fix

--- AUDIOMP3_WIFIino/enviaAudio.js
+++ AUDIOMP3_WIFIino/enviaAudio.js
@@ -2,9 +2,22 @@
 var net = require('net');
 
 var client = new net.Socket();
+var fs = require("fs");
+
+
 client.connect(8090, '192.168.0.43', function() {
 	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
+
+	fs.readFile("1.wav", function (err, data) {
+	    if (err) {
+	    	throw err;
+	    }
+	    
+	    console.log('Enviando audio.',data);
+	    client.write(data);
+	});
+
+	
 });
 
 client.on('data', function(data) {
