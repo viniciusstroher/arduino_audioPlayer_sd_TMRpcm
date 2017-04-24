6b02341256bf2b0550d4ce8ef5c1c27bdf7643e5
Vinicius <viniciusferreirawk@gmail.com>
2017-04-24 15:38:04 -0300

    fix

--- AUDIOMP3_WIFIino/AUDIOMP3_WIFIino.ino
+++ AUDIOMP3_WIFIino/AUDIOMP3_WIFIino.ino
@@ -57,17 +57,18 @@ void setup(){
         Serial.print("set tcp server timout err\r\n");
     }
     
-    Serial.print("setup end\r\n");
-
+    
+    if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+      Serial.println("NO SD DETECTED");
+      return;   // don't do anything more if not
+    }
       /*tmrpcm.speakerPin = 9; //11 on Mega, 9 on Uno, Nano, etc
      
-      if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
-        Serial.println("NO SD DETECTED");
-      return;   // don't do anything more if not
-      }
+      
         // Serial.println("Play audio");
         tmrpcm.volume(1);
        // tmrpcm.play("1.wav"); //the sound file "1" will play each time the arduino powers up, or is reset*/
+    Serial.print("setup end\r\n");
 }
  
 void loop(){  
